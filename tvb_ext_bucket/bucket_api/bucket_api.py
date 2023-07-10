52a8d3c387aa42f922f1b71dbe669c08167e4e82
davidbacter01 <davidbacter01@gmail.com>
2023-07-10 09:32:10 +0300

    WID-229: fix sonar code smells

--- tvb_ext_bucket/bucket_api/bucket_api.py
+++ tvb_ext_bucket/bucket_api/bucket_api.py
@@ -50,7 +50,7 @@ class BucketApiClient(ClientBase):
         self.send_request("DELETE", f"/v1/buckets/{bucket_name}")
 
     def send_request(self, method: str, url: str, *args, **kwargs):
-        hdr, info, sig = self._token.split('.')
+        _, info, _ = self._token.split('.')
         info_json = base64.b64decode(info + '==').decode('utf-8')
 
         # https://www.rfc-editor.org/rfc/rfc7519#section-2
